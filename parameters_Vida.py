4d86318badc5e94587f0ad69d29076c66550825d
Birgit Kriener <BKriener@users.noreply.github.com>
2017-05-29 16:10:35 +0200

    Add files via upload
    Code to generate networks of Wang-Buszaki-type interneurons as described in 
    "Shunting inhibition improves robustness of gamma oscillations 
    in hippocampal interneuron networks by homogenizing firing rates"
    Imre Vida, Marlene Bartos, and Peter Jonas
    Neuron 49, 107-117 (2006) DOI 10.1015/j.neuron.2005.11.036
    
    Adpated to existing NEURON model published in ModelDB:
    https://senselab.med.yale.edu/modeldb/showModel.cshtml?model=21329
    
    Python code adapted and modified from existing code in 
    https://github.com/OpenSourceBrain/OpenCortex
    
    Note: Not yet included is activation of synapses after uncoupled transient
    
    To generate simulation files: python run.py

---
+++ parameters_Vida.py
@@ -0,0 +1,11 @@
+Ri            = 100.    # in Ohm cm
+Cm            = 1.      # in muF cm-2
+Vinit         = -65.    # in mV
+erev          = -65.    # in mV
+gl            = 0.1     # in mS cm-2
+gna_soma      = 35.     # in mS cm-2
+ena_soma      = 55.     # in mV
+gk_soma       = 9.      # in mS cm-2
+ek_soma       = -90.    # in mV
+
+spike_thresh  = 0.      # in mV
